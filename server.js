61531066a3faef28b89756386bb9d6442bf1958a
nerdsnook <hollychappie40@gmail.com>
2016-10-01 16:00:54 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-
-app.get('/', function(req, res) {
-  res.redirect('http://nerdsnook.ddns.net/helpdesk/?p=open');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/admin', function(req, res){
+  res.sendFile(__dirname + '/admin.html');
+});
+
+
+io.on('connection', function(socket){
+  socket.on('help_call', function(msg){
+    io.emit('help_call', msg);
+  });
+});
+
+
 var PORT = process.env.PORT || 3000;
-server.listen(PORT, function(){
-    console.log("Listening on PORT " + process.env.PORT);
+http.listen(PORT, function(){
+    console.log("started Server");
 })
+
+
